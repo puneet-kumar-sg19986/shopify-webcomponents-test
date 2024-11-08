7453440c57ae8fa077508f416db3bd6d6f997130
gurkaran <103989636+puneet-kumar-sg19986@users.noreply.github.com>
2024-11-08 18:55:39 +0530

    Added CSS styling

--- frontend/web/product-card/scripts/main.js
+++ frontend/web/product-card/scripts/main.js
@@ -20,7 +20,53 @@ class ProductCard extends HTMLElement {
   render(data) {
     this.shadowRoot.innerHTML = `
 <style>
+.product-card {
+	width: 220px;
+	background: #edebeb;
+	padding: 25px 5px;
+	border-radius: 4px;
+}
 
+.product__link {
+	text-decoration: none;
+	color: #2a2a2a;
+}
+
+.product-title {
+	margin: 0;
+	text-decoration: none;
+	color: #2a2a2a;
+	font-size: 18px;
+}
+
+.product-info {
+	display: flex;
+	flex-direction: column;
+}
+
+.product-vendor {
+	text-transform: uppercase;
+	font-size: 12px;
+	letter-spacing: 2px;
+}
+
+.product__regular__price {
+	font-weight: 600;
+	font-size: 18px;
+}
+
+.product-img {
+	display: flex;
+	justify-content: center;
+}
+
+.product-img img {
+	width: 150px;
+}
+
+.product-info {
+	padding: 10px;
+}
 </style>
           <div class="product-card ">
               <a class="product__link" href=${data.url}>
